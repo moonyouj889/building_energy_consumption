a58ca01349ec1d4544726e2f6730d8c55e75ef6d
moonyouj889 <moonyouj889@gmail.com>
2019-10-11 11:17:22 -0400

    PubSub running shell script created -- for python2

---
+++ launchPubSub.sh
@@ -0,0 +1,3 @@
+pip install python-dateutil
+
+python ./pubsub/send_meter_data_py2.py --speedFactor 60 --project $PROJECT_ID
--- runDataFlow.sh
+++ runDataFlow.sh
@@ -22,23 +22,23 @@ shift
 TOPIC_IN=energy
 TOPIC_OUT=energy_avgs
 
-echo "Launching $MAIN project=$PROJECT bucket=$BUCKET for BQ dataset=$DATASET and PubSub topic=$TOPIC"
+echo "Launching currentConsumption.py project=$PROJECT_ID bucket=$BUCKET for BQ dataset=$DATASET and PubSub topic=$TOPIC"
 
-python $MAIN \
-    --runner Dataflow Runner \
-    --project $PROJECT \
-    --temp_location gs://$BUCKET/tmp/ \
-    --streaming True \
-    --input_topic projects/$PROJECT/topics/$TOPIC_IN\
-    --output_table_load $DATASET.energy_history\
-    --output_table_stream $DATASET.energy_avgs\ 
-    --output_topic projects/$PROJECT/topics/$TOPIC_OUT\
+# python $MAIN \
+#     --runner Dataflow Runner \
+#     --project $PROJECT \
+#     --temp_location gs://$BUCKET/tmp/ \
+#     --streaming True \
+#     --input_topic projects/$PROJECT/topics/$TOPIC_IN\
+#     --output_table_load $DATASET.energy_history\
+#     --output_table_stream $DATASET.energy_avgs\ 
+#     --output_topic projects/$PROJECT/topics/$TOPIC_OUT\
 
 python ./dataflow/currentConsumption.py \
     --runner DataflowRunner \
-    --project $PROJECT \
+    --project $PROJECT_ID \
     --temp_location gs://$BUCKET/tmp/ \
-    --streaming True \
+    --streaming true \
     --input_topic projects/$PROJECT/topics/energy \
     --output_load_table_suffix buildings.energy_history \
     --output_stream_table buildings.energy_avgs \
