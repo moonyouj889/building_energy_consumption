13c2485082305b77bf2ff89b6ffd0108f5f4d425
moonyouj889 <moonyouj889@gmail.com>
2019-10-12 10:51:44 -0400

    minor changes

--- dataflow/currentConsumption.py
+++ dataflow/currentConsumption.py
@@ -156,11 +156,11 @@ class WindowStartTimestampFn(beam.DoFn):
 
 class AddTimestampDoFn(beam.DoFn):
 
-  def process(self, s):
-    # Extract the timestamp val from string data row
-    # Wrap and emit the current entry and new timestamp in a
-    # TimestampedValue.
-    return window.TimestampedValue(s, s.split(',')[0])
+    def process(self, s):
+        # Extract the timestamp val from string data row
+        # Wrap and emit the current entry and new timestamp in a
+        # TimestampedValue.
+        return beam.transforms.window.TimestampedValue(s, s.split(',')[0])
 
 def run(argv=None):
     '''Build and run the pipeline.'''
@@ -270,7 +270,7 @@ def run(argv=None):
     # second is the building_id, and third is the general meter reading
     avgs = (lines
             #  | 'AddTimestamps' >> beam.Map(lambda s: window.TimestampedValue(s, s.split(',')[0]))
-            | 'AddTimestamps' >>  beam.ParDo(AddTimestampDoFn())
+             | 'AddTimestamps' >>  beam.ParDo(AddTimestampDoFn())
              # | 'SetTimeWindow' >> beam.WindowInto(window.SlidingWindows(WINDOW_SIZE, WINDOW_PERIOD, offset=0))
              | 'SetTimeWindow' >> beam.WindowInto(window.FixedWindows(WINDOW_SIZE, offset=0))
              # splitting to k,v of buildingId (2nd column), general meter reading (3rd column)
